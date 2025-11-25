5803022e170e7f4bf8b99377fde595c916a01cb5
JayfaAnjum <jayfatha62@gmail.com>
2025-11-25 23:46:47 +0530

    create pages and routes

---
+++ client/src/App.jsx
@@ -0,0 +1,43 @@
+
+import './App.css'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import About from './pages/about'
+import Login from './pages/Login'
+import SignUp from './pages/SignUp'
+import Profile from './pages/Profile'
+import Home from './pages/Home'
+
+
+
+function App() {
+  
+
+  return (
+  
+   <BrowserRouter>
+   <Routes>
+    <Route path="/about" element={<About/>} />
+   
+   <Route path="/home" element={<Home/>} />
+    <Route path="/login"  element={<Login/>}/>
+  
+  
+    <Route path="/Signup" element={<SignUp/>} />
+   
+   
+    <Route path="/profile" element={<Profile/>} />
+   </Routes>
+   
+   </BrowserRouter>
+    
+  )
+}
+
+export default App
+//here we console ninja for see console output in vscode instead of web browser
+
+//es7 has lot of features
+
+// tailwindcss intellisense
+
+//prettier
